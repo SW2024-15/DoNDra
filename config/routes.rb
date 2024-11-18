456f27e42941d19d0dc7ad6b517873d47f874f98
Ubuntu <ubuntu@ip-172-31-35-219.ec2.internal>
2024-11-18 04:20:22 +0000

    takata

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+  get 'questions/show'
+  resources :questions, only: [:index, :show]
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
