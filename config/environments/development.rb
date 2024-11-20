b6c8438926c85966434d385a80058b42350ce418
masaya miyazaki <2211140100f@ed.fuk.kindai.ac.jp>
2024-11-20 06:24:30 +0000

    “00000000”

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.hosts << "66a8b2b1f41940609493f0afb8ec0019.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "5f75bd0384514060a7369d10a57ea6b5.vfs.cloud9.us-east-1.amazonaws.com"
 end
