f17a4c76f1ad3c84e0ac1133a576aa0861a56dbe
Ubuntu <ubuntu@ip-172-31-35-219.ec2.internal>
2024-11-20 07:19:23 +0000

    Top

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
+  
   get 'questions/index'
   get 'questions/show'
   resources :questions, only: [:index, :show]
->>>>>>> abe9febff0a6f3c99967216557492e0c5c93d243
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
+  get 'top/main'
+  post 'top/login'
+  root 'top#main'
+  get 'top/logout'
   # Defines the root path route ("/")a
   # root "posts#index"
 end
